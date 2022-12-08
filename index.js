329ff49a26d2e6ef7659f75ac48f62da97647c5c
Shapna <shapna088@gmail.com>
2022-12-08 12:22:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlhnchw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const dataCollection = client.db('blackCoffer').collection('data');
+        
+        app.get('/data', async(req, res)=>{
+            const query = {}
+            const data = await dataCollection.find(query).toArray();
+            res.send(data)
+        })
+    } finally {
+      
+    }
+  }
+  run().catch(err=> console.log(err));
+
+app.get('/', async (req, res) => {
+    res.send('server is running');
+})
+
+app.listen(port, async (req, res) => {
+    console.log(`Server is running on port ${port}`);
+})
